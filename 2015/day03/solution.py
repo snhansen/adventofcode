61346a070dcceaec60c1b58c2ddbef13676f028b
Stefan Nygaard Hansen <stefanh@outlook.com>
2020-09-01 21:09:42 +0200

    Added day03, 2015

--- 2015/day03/solution.py
+++ 2015/day03/solution.py
@@ -9,3 +9,16 @@ for x in inp:
     visited.append(visited[-1]+dirs[x])
     
 print(len(set(visited)))
+
+# Part 2
+visit1 = [0]
+visit2 = [0]
+for i, x in enumerate(inp):
+    if i%2 == 0:
+        visit1.append(visit1[-1]+dirs[x])
+    else:
+        visit2.append(visit2[-1]+dirs[x])
+
+print(len(set(visit1+visit2)))
+
+    
