643f77d3989c355f3d759e082524bd9f9b354778
Stefan Nygaard Hansen <stefanh@outlook.com>
2020-09-13 10:36:36 +0200

    Added day 23, 2019

---
+++ 2019/day23/solution.py
@@ -0,0 +1,63 @@
+import sys
+import os
+from itertools import product
+sys.path.insert(1, os.path.dirname(os.getcwd()))
+from aoc_functions import intcode_machine
+
+with open('input') as f:
+    inp = list(map(int, f.read().split(',')))
+
+n = 50
+comps = [intcode_machine(inp, [i, -1]) for i in range(n)]
+
+def packets_to_send():
+    packets = []
+    for i in range(n):
+        while True:
+            try:
+                _, output = comps[i].run_till_output_or_halt()
+                packets.append(output)
+            except IndexError:
+                break
+    return [[packets[k+i] for i in range(3)] for k in range(0, len(packets), 3)]
+
+def send_packets(ps):
+    for p in ps:
+        comps[p[0]].add_inputs([p[1], p[2]])
+
+# Part 1
+break_var = False
+while True:
+    packets = packets_to_send()
+    for p in packets:
+        if p[0] == 255:
+            print(p[2])
+            break_var = True
+    if break_var:
+        break
+    send_packets(packets)
+
+# Part 2
+comps = [intcode_machine(inp, [i, -1]) for i in range(n)]
+
+break_var = False
+nat_packet = None
+last_nat_packet = None
+
+while True:
+    packets = packets_to_send()
+    if not packets:
+        if nat_packet == last_nat_packet and nat_packet:
+            print(nat_packet[1])
+            break_var = True
+        comps[0].add_inputs(nat_packet)
+        last_nat_packet = nat_packet
+        nat_packet = None
+    else:
+        for p in packets:
+            if p[0] == 255:
+                nat_packet = p[1:3]
+        packets = [p for p in packets if p[0] != 255]
+        send_packets(packets)
+    if break_var:
+        break
