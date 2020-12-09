2dbd4249c92f5ee51d54dd74a8eb43fe7e37662c
Stefan Nygaard Hansen <au234616@uni.au.dk>
2020-12-09 08:09:16 +0100

    Added day09, 2020

---
+++ 2020/day09/solution.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+
+with open('input') as f:
+    inp = list(map(int, f.readlines()))
+
+# Part 1
+i = 25
+while True:
+    valid = False
+    for y, z in combinations(inp[i-25:i], 2):
+        if inp[i] == y + z:
+            valid = True
+    if not valid:
+        inv = inp[i]
+        break
+    i += 1
+
+print(inv)
+
+# Part 2
+for i in range(len(inp)):
+    for j in range(i+1, len(inp)):
+        ls = inp[i:j+1]
+        if sum(ls) == inv:
+            print(min(ls)+max(ls))
