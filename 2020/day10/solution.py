d900a7f41a9fbd920074d655c956dd07c1868a53
Stefan Nygaard Hansen <au234616@uni.au.dk>
2020-12-10 12:01:25 +0100

    Tidied up the solution a bit

--- 2020/day10/solution.py
+++ 2020/day10/solution.py
@@ -1,17 +1,16 @@
-from collections import defaultdict
 with open('input') as f:
     inp = list(map(int, f.readlines()))
 
 inp.append(0)
 inp.append(max(inp)+3)
 inp.sort()
-diff = [x-y for x,y in zip(inp[1:], inp[:len(inp)-1])]
+diff = [x - y for x, y in zip(inp[1:], inp[:len(inp)-1])]
 
 # Part 1
-print(len([x for x in diff if x == 3])*len([x for x in diff if x == 1]))
+print(diff.count(3)*diff.count(1))
 
 # Part 2
-cache = defaultdict()
+cache = {}
 cache[0] = 1
 
 def no_ways(i):
