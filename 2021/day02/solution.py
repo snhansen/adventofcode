bf645af8144b1f44c9ad88c74cdcd7f2c0f587e5
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2021-12-02 08:45:27 +0100

    Updated solution slightly

--- 2021/day02/solution.py
+++ 2021/day02/solution.py
@@ -6,19 +6,18 @@ dirs = {'forward': 1, 'down': -1j, 'up': 1j}
 pos = 0
 
 for x in inp:
-    d = x.strip().split(' ')[0]
-    v = int(x.strip().split(' ')[1])
+    d, v = x.strip().split()
+    v = int(v)
     pos += v*dirs[d]
 
 print(int(abs(pos.imag)*abs(pos.real)))
 
 # Part 2
-aim = 0
-pos = 0
+aim = pos = 0
 
 for x in inp:
-    d = x.strip().split(' ')[0]
-    v = int(x.strip().split(' ')[1])
+    d, v = x.strip().split()
+    v = int(v)
     if d != 'forward':
         aim = aim+v if d == 'down' else aim-v
     else:
