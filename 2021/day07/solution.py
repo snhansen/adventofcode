c22f49e536ff70680269609710324c36c37f7dc0
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2021-12-07 08:06:19 +0100

    Added day07, 2021

---
+++ 2021/day07/solution.py
@@ -0,0 +1,16 @@
+import math
+
+with open('input') as f:
+    ls = list(map(int, f.read().split(',')))
+
+min_dist1, min_dist2 = math.inf, math.inf
+for y in range(min(ls), max(ls)+1):
+    dist1 = sum([abs(x-y) for x in ls])
+    if dist1 < min_dist1:
+        min_dist1 = dist1
+    dist2 = sum([int(abs(x-y)*(abs(x-y)+1)/2) for x in ls])
+    if dist2 < min_dist2:
+        min_dist2 = dist2
+    
+print(min_dist1)
+print(min_dist2)
