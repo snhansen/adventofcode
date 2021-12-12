8a7d0c7d927eec6b62a46f0272f3362ef5229d2e
Stefan Nygaard Hansen <stefanh@outlook.com>
2021-12-12 12:16:06 +0100

    Added day12, 2021

---
+++ 2021/day12/solution.py
@@ -0,0 +1,38 @@
+from collections import defaultdict, deque
+
+with open('input') as f:
+    inp = f.read().split()
+
+goto = defaultdict(list)
+
+for l in inp:
+    x, y = l.split('-')
+    if y != 'start':
+        goto[x].append(y)
+    if x != 'start':
+        goto[y].append(x)
+
+to_check = deque()
+to_check.append((['start'], False))
+paths = []
+
+while to_check:
+    path, twice = to_check.popleft()
+    if 'end' in path:
+        paths.append((path, twice))
+    else:
+        for x in goto[path[-1]]:
+            criterion = x.islower() and x in path
+            if twice and criterion:
+                continue
+            if not twice and criterion:
+                to_check.append((path + [x], True))
+                continue
+            else:
+                to_check.append((path + [x], twice))
+
+# Part 1
+print(len([1 for p, twice in paths if not twice]))
+
+# Part 2
+print(len(paths))
