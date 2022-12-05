c84e7c173f1aebf3e32cee1222a7850faedeb7dd
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2022-12-05 08:24:44 +0100

    Added day05, 2022

---
+++ 2022/day05/solution.py
@@ -0,0 +1,39 @@
+from collections import deque, defaultdict
+import re
+import copy
+
+with open("input") as f:
+    inp = f.readlines()
+
+stacks = [[] for _ in range(len(inp[0])//4)]
+moves = []
+for line in inp:
+    for i in range(0,len(line),4):
+        if line[i] == "[":
+            stacks[i//4].append(line[i+1])
+    if "move" in line:
+        moves.append(list(map(int, re.findall("\d+", line))))
+
+stacks = list(map(deque, stacks))
+
+# Part 1
+
+stacks_cp = copy.deepcopy(stacks)
+for move in moves:
+    n, fr, to = move
+    for _ in range(n):
+        stacks_cp[to-1].appendleft(stacks_cp[fr-1].popleft())
+
+print(''.join([stacks_cp[i][0] for i in range(len(stacks_cp))]))
+
+# Part 2
+
+stacks_cp = copy.deepcopy(stacks)
+for move in moves:
+    n, fr, to = move
+    stack = []
+    for _ in range(n):
+        stack.append(stacks_cp[fr-1].popleft())
+    stacks_cp[to-1].extendleft(reversed(stack))
+
+print(''.join([stacks_cp[i][0] for i in range(len(stacks_cp))]))
