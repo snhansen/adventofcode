e30b94627021adc4f1f4273a52cfcd1b5e59811f
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2022-12-08 08:27:39 +0100

    Added cleaned up solution

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -1,26 +1,17 @@
 with open("input") as f:
-    inp = f.read()
+    inp = f.read().strip().split("\n")
 
-
-grid = {}
-i, j = 0, 0
-for c in inp:
-    if c == "\n":
-        j += 1
-        i = 0
-    else:
-        grid[i+j*1j] = int(c)
-        i += 1
-
-n = int(len(grid)**(0.5))
+rows = len(inp)
+cols = len(inp[0])
+grid = {i + j * 1j: int(inp[i][j]) for i in range(rows) for j in range(cols)}
 
 # Part 1
 def is_visible(p):
-    if (p.imag == 0) or (p.real == 0) or (p.imag == n) or (p.real == n):
+    if (p.imag == 0) or (p.real == 0) or (p.imag == rows) or (p.real == cols):
         return True
-    for dir in [-1, 1, 1j, -1j]:
-        trees = [p + dir*k for k in range(1, n) if p+dir*k in grid]
-        if all(grid[tree] < grid[p] for tree in trees):
+    for dir in (-1, 1, 1j, -1j):
+        to_check = (p + dir*k for k in range(1, max(rows, cols)) if p + dir*k in grid)
+        if all(grid[tree] < grid[p] for tree in to_check):
             return True
     return False
 
@@ -30,8 +21,8 @@ print(sum(is_visible(p) for p in grid))
 # Part 2
 def scenic_core(p):
     res = 1
-    for dir in [-1, 1, 1j, -1j]:
-        for k in range(1, n):
+    for dir in (-1, 1, 1j, -1j):
+        for k in range(1, max(rows, cols)):
             p_new = p + dir*k
             if p_new not in grid:
                 k -= 1
