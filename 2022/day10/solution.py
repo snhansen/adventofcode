ca746123b408f27a2679ad962b5920abc084d8bc
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2022-12-10 08:01:09 +0100

    Added day10, 2022

--- 2022/day10/solution.py
+++ 2022/day10/solution.py
@@ -1,21 +1,13 @@
 with open("input") as f:
     inp = f.read().strip().split("\n")
 
-vals = [1, 1]
-i = 0
-while True:
-    instr = inp[i%len(inp)]
+vals = [1]
+for instr in inp:
     val = vals[-1]
+    vals.append(val)
     match instr.split(" "):
-        case ["noop"]:
-            vals.append(val)
         case [_, x]:
-            val += int(x)
-            vals.append(val)
-            vals.append(val)
-    i += 1
-    if len(vals) > 240:
-        break
+            vals.append(val + int(x))
 
 # Part 1
 print(sum((i*vals[i-1] for i in (20, 60, 100, 140, 180, 220))))
@@ -26,8 +18,7 @@ for row in range(6):
     pixels = []
     for cycle in range(40):
         x = vals[row*40 + cycle]
-        sprite = range(x-1,x+2)
-        draw = "#" if cycle in sprite else "."
+        draw = "#" if abs(cycle - x) <= 1 else "."
         pixels.append(draw)
     screen.append(pixels)
 
