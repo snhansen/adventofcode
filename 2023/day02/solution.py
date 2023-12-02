cbea210ca44fd79b009760241ee5c25955d2c40c
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2023-12-02 08:05:53 +0100

    Update solution

--- 2023/day02/solution.py
+++ 2023/day02/solution.py
@@ -1,37 +1,27 @@
-import re
-
 with open("input") as f:
     inp = f.read().strip().split("\n")
 
 games = []
-for line in inp:
-    game, grabs = line.split(": ")
-    game = re.search("\d+", game).group()
+for game, line in enumerate(inp):
+    _, grabs = line.split(": ")
     grabs = grabs.replace(";", ",")
     grabs = grabs.split(", ")
     grabs = [(int(grab.split()[0]), grab.split()[1]) for grab in grabs]
-    games.append((int(game), grabs))
+    games.append((game+1, grabs))
 
 # Part 1
 def valid_game(ls):
     cubes = {"red": 12, "green": 13, "blue": 14}
-    for val, color in ls:
-        if val > cubes[color]:
-            return False
-    return True
+    return all(val <= cubes[color] for val, color in ls)
 
 print(sum([game for game, grabs in games if valid_game(grabs)]))
 
 # Part 2
-def min_cubes(grabs):
+def power(grabs):
     cubes = {"red": 0, "green": 0, "blue": 0}
     for val, color in grabs:
         cubes[color] = max(cubes[color], val)
-    return list(cubes.values())
+    vals = list(cubes.values())
+    return vals[0]*vals[1]*vals[2]
 
-power = 0
-for game, grabs in games:
-    red, green, blue = min_cubes(grabs)
-    power += red*green*blue
-    
-print(power)
+print(sum([power(grabs) for _, grabs in games]))
