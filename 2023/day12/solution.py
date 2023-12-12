efeb4cfab07cee46b06a7c3e223591efde908a4a
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2023-12-12 21:18:08 +0100

    Update solution

--- 2023/day12/solution.py
+++ 2023/day12/solution.py
@@ -7,51 +7,38 @@ with open("input") as f:
 @cache
 def get_arr(s, n):
     s = s.rstrip(".").lstrip(".")
-    if not s and not n:
-        return 1
-    if not s and n:
-        return 0
-    if s and not n:
-        if s.count("#") > 0:
-            return 0
-        else:
-            return 1
+    if not s:
+        return not n
+    if not n:
+        return "#" not in s
     if s[0] == "?":
         return get_arr("." + s[1:], n) + get_arr("#" + s[1:], n)
     elif s[0] == "#":
-        new_s = list(s)
-        new_n = list(n)
-        n = new_n.pop(0)
-        if len(s) < n:
+        n = list(n)
+        n0 = n.pop(0)
+        if len(s) < n0:
             return 0
-        if any(x == "." for x in s[:n]):
+        if any(x == "." for x in s[:n0]):
             return 0
         try: 
-            if s[n] == "#":
+            if s[n0] == "#":
                 return 0
         except IndexError:
             pass
-        new_s = "".join(new_s[(n+1):])
-        return get_arr(new_s, tuple(new_n))
+        s = "".join(s[(n0+1):])
+        return get_arr(s, tuple(n))
 
-# Part 1
+
+# Part 1 + 2
 res = 0
+part1, part2 = 0, 0
 for line in inp:
     springs, n = line.split()
-    n = tuple(map(int, n.split(",")))
-    res += get_arr(springs, n)
+    n = list(map(int, n.split(",")))
+    part1 += get_arr(springs, tuple(n))
+    springs = "?".join([springs]*5)
+    n = n*5
+    part2 += get_arr(springs, tuple(n))
 
-print(res)
-
-# Part 2
-res = 0
-for line in inp:
-    temp, n = line.split()
-    springs = ""
-    for _ in range(4):
-        springs += temp + "?"
-    springs += temp
-    n = tuple(list(map(int, n.split(",")))*5)
-    res += get_arr(springs, n)
-
-print(res)
+print(part1)
+print(part2)
