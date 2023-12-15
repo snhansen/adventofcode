7cf07744fd6a3a0889d1ce0118f33cfc932533c6
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2023-12-15 14:53:02 +0100

    Tidy up solution

--- 2023/day15/solution.py
+++ 2023/day15/solution.py
@@ -11,14 +11,14 @@ def get_val(seq):
     for x in seq:
         val += ord(x)
         val *= 17
-        val = val % 256
+        val %= 256
     return val
 
 # Part 1
 print(sum(map(get_val, steps)))
  
 # Part 2 
-boxes = defaultdict(lambda: defaultdict(list))
+boxes = defaultdict(dict)
 
 for step in steps:
     label = re.findall("(\w+)[=-]", step)[0]
@@ -26,26 +26,17 @@ for step in steps:
     box = get_val(label)
     if oper == "=":
         n = re.findall("\d+", step)[0]
-        try:
-            i = boxes[box]["label"].index(label)
-            boxes[box]["focal"][i] = int(n)
-        except ValueError:
-            boxes[box]["label"].append(label)
-            boxes[box]["focal"].append(int(n))
+        boxes[box][label] = int(n)
     elif oper == "-":
-        try:
-            i = boxes[box]["label"].index(label)
-            boxes[box]["label"].pop(i)
-            boxes[box]["focal"].pop(i)
-        except ValueError:
-            pass
+        if label in boxes[box]:
+            boxes[box].pop(label)   
 
 
-ans = 0
-for box in range(256):
-    if not boxes[box]["label"]:
-        continue
-    for slot, focal in enumerate(boxes[box]["focal"]):
-        ans += (box+1)*(slot+1)*focal
-
-print(ans)     
+  
+print(
+    sum(
+        (box + 1)*(slot + 1)*focal 
+        for box in boxes.keys()
+        for slot, (_, focal) in enumerate(boxes[box].items())
+    )
+)
