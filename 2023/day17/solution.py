3c36b55802f7a6f1c84a528724a1d0bc1c5b0db5
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2023-12-17 14:26:36 +0100

    Clean up solution

--- 2023/day17/solution.py
+++ 2023/day17/solution.py
@@ -1,5 +1,4 @@
 from heapq import heappop, heappush
-from collections import defaultdict
 
 with open("input") as f:
     inp = f.read().strip().split("\n")
@@ -11,10 +10,13 @@ corner = (len(inp[0]) - 1) + (len(inp) - 1)*1j
 
 # state = (heat, id, position, direction_moved, moved_blocks)
 q = [(grid[p], id(p), p, p, 1) for p in [1, 1j]]
-min_heat = defaultdict(lambda: float("inf"))
+seen = set()
 
 while q:
     heat, _, p, dp, blocks = heappop(q)
+    if (p, dp, blocks) in seen:
+        continue
+    seen.add((p, dp, blocks))
     if p == corner:
         print(heat)
         break
@@ -24,27 +26,24 @@ while q:
     for new_dp in dirs:
         new_p = p + new_dp
         if new_p in grid:
-            to_push.append((heat + grid[new_p], new_p, new_dp, 1))
+            heappush(q, (heat + grid[new_p], id(new_p), new_p, new_dp, 1))
     # If moved less than 3 blocks we can continue in the same direction.
     if blocks < 3:
         new_p = p + dp
         if new_p in grid:
-            to_push.append((heat + grid[new_p], new_p, dp, blocks + 1))
-    # Add all states to queue if heat is lower.
-    for (heat, p, dp, blocks) in to_push:
-        if heat < min_heat[(p, dp, blocks)]:
-            heappush(q, (heat, id(p), p, dp, blocks))
-            min_heat[(p, dp, blocks)] = heat
-
+            heappush(q, (heat + grid[new_p], id(new_p), new_p, dp, blocks + 1))
 
 # Part 2
 
 # state = (heat, id, position, direction_moved, moved_blocks)
 q = [(grid[p], id(p), p, p, 1) for p in [1, 1j]]
-min_heat = defaultdict(lambda: float("inf"))
+seen = set()
 
 while q:
     heat, _, p, dp, blocks = heappop(q)
+    if (p, dp, blocks) in seen:
+        continue
+    seen.add((p, dp, blocks))
     if (p == corner) & (blocks >= 4):
         print(heat)
         break
@@ -53,16 +52,11 @@ while q:
     if blocks < 10:
         new_p = p + dp
         if new_p in grid:
-            to_push.append((heat + grid[new_p], new_p, dp, blocks + 1))
+            heappush(q, (heat + grid[new_p], id(new_p), new_p, dp, blocks + 1))
     # If moved at least 4 blocks, we can turn.
     if blocks >= 4:
         dirs = [dp*1j, -dp*1j]
         for new_dp in dirs:
             new_p = p + new_dp
             if new_p in grid:
-                to_push.append((heat + grid[new_p], new_p, new_dp, 1))
-    # Add all states to queue if heat is lower.
-    for (heat, p, dp, blocks) in to_push:
-        if heat < min_heat[(p, dp, blocks)]:
-            heappush(q, (heat, id(p), p, dp, blocks))
-            min_heat[(p, dp, blocks)] = heat
+                heappush(q, (heat + grid[new_p], id(new_p), new_p, new_dp, 1))
