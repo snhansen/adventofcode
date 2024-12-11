ed52fa75b788b571178a5e458f3c9999805ed87c
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2024-12-11 07:42:25 +0100

    Add day 11, 2024

---
+++ 2024/day11/solution.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+with open("input") as f:
+    inp = f.read().strip()
+
+stones = list(map(int, inp.split(" ")))
+
+
+@cache
+def get_no_stones(stone, n):
+    if n == 0:
+        return 1
+    if stone == 0:
+        res = get_no_stones(1, n - 1)
+    elif len(str(stone)) % 2 == 0:
+        str_stone = str(stone)
+        mid = len(str_stone) // 2
+        res = get_no_stones(int(str_stone[:mid]), n - 1) + get_no_stones(int(str_stone[mid:]), n - 1)
+    else:
+        res = get_no_stones(stone*2024, n - 1)
+    return res
+
+
+# Part 1
+print(sum(get_no_stones(stone, 25) for stone in stones))
+
+# Part 2
+print(sum(get_no_stones(stone, 75) for stone in stones))
