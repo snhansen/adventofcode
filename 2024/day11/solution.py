7e82d24434d7ec2b2f91e3615ecc4b6b633ad31e
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2024-12-11 09:12:23 +0100

    Clean up solution

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -11,13 +11,12 @@ def get_no_stones(stone, n):
     if n == 0:
         return 1
     if stone == 0:
-        res = get_no_stones(1, n - 1)
+        return get_no_stones(1, n - 1)
     elif len(s:= str(stone)) % 2 == 0:
         mid = len(s) // 2
-        res = get_no_stones(int(s[:mid]), n - 1) + get_no_stones(int(s[mid:]), n - 1)
+        return get_no_stones(int(s[:mid]), n - 1) + get_no_stones(int(s[mid:]), n - 1)
     else:
-        res = get_no_stones(stone*2024, n - 1)
-    return res
+        return get_no_stones(stone*2024, n - 1)
 
 
 # Part 1
