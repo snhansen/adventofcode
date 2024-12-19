d8403533a6df62d8db0804d8b3491758638e05b1
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2024-12-19 09:38:28 +0100

    Simplify solution

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -11,14 +11,7 @@ designs = inp2.split("\n")
 def no_ways(design):
     if design == "":
         return 1
-    to_check = []
-    for pattern in patterns:
-        if design[:len(pattern)] == pattern:
-            to_check.append(pattern)
-    if not to_check:
-        return 0
-    else:
-        return sum(no_ways(design[len(pattern):]) for pattern in to_check)
+    return sum(no_ways(design[len(pattern):]) for pattern in patterns if design[:len(pattern)] == pattern)
 
 # Part 1
 print(sum(no_ways(design) > 0 for design in designs))
