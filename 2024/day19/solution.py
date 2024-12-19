990a4af927fb0d88e0eef5b4b5413bb118028c93
Stefan Nygaard Hansen <stefannygaardhansen@gmail.com>
2024-12-19 09:00:24 +0100

    Add day 19, 2024

---
+++ 2024/day19/solution.py
@@ -0,0 +1,27 @@
+from functools import cache
+
+with open("input") as f:
+    inp1, inp2 = f.read().strip().split("\n\n")
+
+patterns = inp1.split(", ")
+designs = inp2.split("\n")
+
+
+@cache
+def no_ways(design):
+    if design == "":
+        return 1
+    to_check = []
+    for pattern in patterns:
+        if design[:len(pattern)] == pattern:
+            to_check.append(pattern)
+    if len(to_check) == 0:
+        return 0
+    else:
+        return sum(no_ways(design[len(pattern):]) for pattern in to_check)
+
+# Part 1
+print(sum(no_ways(design) > 1 for design in designs))
+
+# Part 2
+print(sum(no_ways(design) for design in designs))
